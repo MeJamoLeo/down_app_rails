ad3dfb2a22a5fa935c6ac090bdbbcb0d81692607
MeJamoLeo <reonarudo11919273@gmail.com>
2020-07-12 16:57:30 +0900

    コントローラの修正

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,10 +1,9 @@
 require 'test_helper'
 
-class UserControllerTest < ActionDispatch::IntegrationTest
+class UsersControllerTest < ActionDispatch::IntegrationTest
   test "should get signup" do
     get signup_path
     assert_response :success
     assert_select "title", full_title("Sign Up")
   end
-
 end
